76040df59c7de6b54215b9a83fd9e614450cca00
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-01-20 00:07:00 +0200

    add

--- src/containers/jumbotron.jsx
+++ src/containers/jumbotron.jsx
@@ -1,7 +1,6 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import jumboData from "../fixtures/jumbo.json"
-import { Jumbotron } from "../components/jumbotron";
+import Jumbotron from "../components/jumbotron";
 
 export function JumbotronContainer() {
     return (
