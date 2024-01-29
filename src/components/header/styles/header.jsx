84ea9da528917bd8809da0774e42be86612b44ad
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-01-29 03:31:40 +0200

    add

--- src/components/header/styles/header.jsx
+++ src/components/header/styles/header.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { Link as ReachRouterLink } from 'react-router-dom';
+import homeBg from "../../../../images/misc/home-bg.jpg"
 
 export const Background = styled.section`
     display: flex;
     flex-direction: column;
-    background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../../../../images/misc/home-bg.jpg')});
+    background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : homeBg)});
     background-size: ${( {src} ) => src ? "contain" : "cover" };
     background-repeat: no-repeat;
 
