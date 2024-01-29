5e8d3239dbf4773a3db8c6d1e29c453be3b6e552
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-01-29 03:23:00 +0200

    add

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../components/header";
 import * as ROUTES from "../constants/routes"
+import netflixLogo from "../../images/misc/logo.svg"
 
 export function HeaderContainer({ children }) {
     return (
         <Header>
             <Header.Frame>
-                <Header.Logo to={ROUTES.HOME} src="../../images/misc/logo.svg" alt="Netflix logo"/>
+                <Header.Logo to={ROUTES.HOME} src={netflixLogo} alt="Netflix logo"/>
                 <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign in</Header.ButtonLink>
             </Header.Frame>
             {children}
