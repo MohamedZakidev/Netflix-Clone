a8b3c96627a5441899a84876ce39fd2ec1abe3c5
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-04-24 14:48:44 +0200

    Update useAuthListener.jsx

--- src/hooks/useAuthListener.jsx
+++ src/hooks/useAuthListener.jsx
@@ -5,7 +5,7 @@ import { FirebaseContext } from '../context/firebase';
 export default function useAuthListener() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
     const { firebase } = useContext(FirebaseContext);
-    
+
     useEffect(() => {
         const listener = firebase.auth().onAuthStateChanged((authUser) => {
             if (authUser) {
@@ -16,9 +16,9 @@ export default function useAuthListener() {
                 setUser(null);
             }
         });
-        
+
         return () => listener();
     }, []);
-    
+
     return { user };
 }
