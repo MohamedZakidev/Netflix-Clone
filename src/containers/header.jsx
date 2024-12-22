32bb67055c7cf974f4f8b2b8f055b932499f0b24
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-12-22 05:08:01 +0200

    Update header.jsx

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/header";
 import * as ROUTES from "../constants/routes";
-import netflixLogo from "/misc/movlix.jpg";
+import netflixLogo from "/misc/logo.svg";
 
 export function HeaderContainer({ children }) {
     return (
